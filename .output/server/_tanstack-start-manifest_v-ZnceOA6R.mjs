95fee7c04668e3e08d545e49fa628c0f5e260ade
omar232208 <omarmg831@gmail.com>
2026-07-10 17:40:12 +0300

    Add .output/server/_tanstack-start-manifest_v-ZnceOA6R.mjs

---
+++ .output/server/_tanstack-start-manifest_v-ZnceOA6R.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-ZnceOA6R.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "C:/Users/omarm_h/Downloads/html-friend-finder-main/html-friend-finder-main/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-DhB2qDXz.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-DhB2qDXz.js"
+		} }]
+	},
+	"/": {
+		filePath: "C:/Users/omarm_h/Downloads/html-friend-finder-main/html-friend-finder-main/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-vxBbjSzD.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
