947463a7f10f548f67f4ccb1f991494f7c2bb28b
omar232208 <omarmg831@gmail.com>
2026-07-10 17:40:20 +0300

    Add osiris-plugin/build.bat

---
+++ osiris-plugin/build.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Building OsirisServerPlugin...
+echo.
+echo Make sure you have Java 17+ and Maven installed!
+echo.
+mvn clean package
+echo.
+echo If successful, the plugin JAR will be in: target\osiris-server-plugin-1.0.0.jar
+pause
